683b72b50ecda153dad63b0d5d1fdb7057bf7390
p013570 <p013570@users.noreply.github.com>
2017-04-06 11:42:34 +0100

    gh-3 - renamed the package names for the implementations

---
+++ src/test/java/uk/gov/gchq/koryphe/impl/binaryoperator/ProductTest.java
@@ -0,0 +1,360 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.koryphe.impl.binaryoperator;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.gov.gchq.koryphe.binaryoperator.BinaryOperatorTest;
+import uk.gov.gchq.koryphe.util.JsonSerialiser;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ProductTest extends BinaryOperatorTest {
+    private Number state;
+
+    @Before
+    public void before() {
+        state = null;
+    }
+
+    @Test
+    public void testAggregateInIntMode() {
+        // Given
+        final Product product = new Product();
+
+        // When 1
+        state = product.apply(2, state);
+
+        // Then 1
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+
+        // When 2
+        state = product.apply(3, state);
+
+        // Then 2
+        assertTrue(state instanceof Integer);
+        assertEquals(6, state);
+
+        // When 3
+        state = product.apply(8, state);
+
+        // Then 3
+        assertTrue(state instanceof Integer);
+        assertEquals(48, state);
+    }
+
+    @Test
+    public void testAggregateInIntModeMixedInput() {
+        // Given
+        final Product product = new Product();
+
+        // When 1
+        state = product.apply(2, state);
+
+        // Then 1
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+
+        // When 2
+        try {
+            state = product.apply(2.7d, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 2
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+
+        // When 3
+        try {
+            state = product.apply(1l, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 3
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+    }
+
+    @Test
+    public void testAggregateInLongMode() {
+        // Given
+        final Product product = new Product();
+
+        // When 1
+        state = product.apply(2l, state);
+
+        // Then 1
+        assertTrue(state instanceof Long);
+        assertEquals(2l, state);
+
+        // When 2
+        state = product.apply(1l, state);
+
+        // Then 2
+        assertTrue(state instanceof Long);
+        assertEquals(2l, state);
+
+        // When 3
+        state = product.apply(3l, state);
+
+        // Then 3
+        assertTrue(state instanceof Long);
+        assertEquals(6l, state);
+    }
+
+    @Test
+    public void testAggregateInLongModeMixedInput() {
+        // Given
+        final Product product = new Product();
+        state = 1l;
+
+        // When 1
+        try {
+            state = product.apply(1, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 1
+        assertEquals(1l, state);
+
+        // When 2
+        state = product.apply(3l, state);
+
+        // Then 2
+        assertTrue(state instanceof Long);
+        assertEquals(3l, state);
+
+        // When 3
+        try {
+            state = product.apply(2.5d, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 3
+        assertTrue(state instanceof Long);
+        assertEquals(3l, state);
+    }
+
+    @Test
+    public void testAggregateInDoubleMode() {
+        // Given
+        final Product product = new Product();
+
+        // When 1
+        state = product.apply(1.2d, state);
+
+        // Then 1
+        assertTrue(state instanceof Double);
+        assertEquals(1.2d, state);
+
+        // When 2
+        state = product.apply(2.5d, state);
+
+        // Then 2
+        assertTrue(state instanceof Double);
+        assertEquals(3.0d, state);
+
+        // When 3
+        state = product.apply(1.5d, state);
+
+        // Then 3
+        assertTrue(state instanceof Double);
+        assertEquals(4.5d, state);
+    }
+
+    @Test
+    public void testAggregateInDoubleModeMixedInput() {
+        // Given
+        final Product product = new Product();
+        state = 1d;
+
+        // When 1
+        try {
+            state = product.apply(1, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 1
+        assertEquals(1d, state);
+
+        // When 2
+        try {
+            state = product.apply(3l, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 2
+        assertEquals(1d, state);
+
+        // When 3
+        state = product.apply(2.1d, state);
+
+        // Then 3
+        assertTrue(state instanceof Double);
+        assertEquals(2.1d, state);
+    }
+
+    @Test
+    public void testAggregateInAutoModeIntInputFirst() {
+        // Given
+        final Product product = new Product();
+        state = 1;
+
+        // When 1
+        state = product.apply(2, state);
+
+        // Then 1
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+
+        // When 2
+        try {
+            state = product.apply(3l, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 2
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+
+        // When 3
+        try {
+            state = product.apply(2.1d, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 3
+        assertTrue(state instanceof Integer);
+        assertEquals(2, state);
+    }
+
+    @Test
+    public void testAggregateInAutoModeLongInputFirst() {
+        // Given
+        final Product product = new Product();
+        state = 1l;
+
+        // When 1
+        state = product.apply(2l, state);
+
+        // Then 1
+        assertTrue(state instanceof Long);
+        assertEquals(2l, state);
+
+        // When 2
+        try {
+            state = product.apply(3, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 2
+        assertTrue(state instanceof Long);
+        assertEquals(2l, state);
+
+        // When 3
+        try {
+            state = product.apply(2.1d, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 3
+        assertTrue(state instanceof Long);
+        assertEquals(2l, state);
+    }
+
+    @Test
+    public void testAggregateInAutoModeDoubleInputFirst() {
+        // Given
+        final Product product = new Product();
+        state = 1d;
+
+        // When 1
+        state = product.apply(1.1d, state);
+
+        // Then 1
+        assertTrue(state instanceof Double);
+        assertEquals(1.1d, state);
+
+        // When 2
+        try {
+            state = product.apply(2, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 2
+        assertTrue(state instanceof Double);
+        assertEquals(1.1d, state);
+
+        // When 3
+        try {
+            state = product.apply(1l, state);
+            fail();
+        } catch (final ClassCastException cce) {
+        }
+
+        // Then 3
+        assertTrue(state instanceof Double);
+        assertEquals(1.1d, state);
+    }
+
+
+    @Test
+    public void shouldJsonSerialiseAndDeserialise() throws IOException {
+        // Given
+        final Product aggregator = new Product();
+
+        // When 1
+        final String json = JsonSerialiser.serialise(aggregator);
+
+        // Then 1
+        JsonSerialiser.assertEquals(String.format("{%n" +
+                "  \"class\" : \"uk.gov.gchq.koryphe.impl.binaryoperator.Product\"%n" +
+                "}"), json);
+
+        // When 2
+        final Product deserialisedAggregator = JsonSerialiser.deserialise(json, Product.class);
+
+        // Then 2
+        assertNotNull(deserialisedAggregator);
+    }
+
+    @Override
+    protected Product getInstance() {
+        return new Product();
+    }
+
+    @Override
+    protected Class<Product> getFunctionClass() {
+        return Product.class;
+    }
+}
---
+++ src/test/java/uk/gov/gchq/koryphe/impl/predicate/IsXMoreThanYTest.java
@@ -0,0 +1,80 @@
+package uk.gov.gchq.koryphe.impl.predicate;
+
+import org.junit.Test;
+import uk.gov.gchq.koryphe.predicate.PredicateTest;
+import uk.gov.gchq.koryphe.util.JsonSerialiser;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class IsXMoreThanYTest extends PredicateTest {
+    @Test
+    public void shouldAcceptWhenMoreThan() {
+        // Given
+        final IsXMoreThanY filter = new IsXMoreThanY();
+
+        // When
+        boolean accepted = filter.test(2, 1);
+
+        // Then
+        assertTrue(accepted);
+    }
+
+    @Test
+    public void shouldRejectTheValueWhenMoreThan() {
+        // Given
+        final IsXMoreThanY filter = new IsXMoreThanY();
+
+        // When
+        boolean accepted = filter.test(5, 6);
+
+        // Then
+        assertFalse(accepted);
+    }
+
+
+    @Test
+    public void shouldRejectTheValueWhenEqualTo() {
+        // Given
+        final IsXMoreThanY filter = new IsXMoreThanY();
+
+        // When
+        boolean accepted = filter.test(5, 5);
+
+        // Then
+        assertFalse(accepted);
+    }
+
+    @Test
+    public void shouldJsonSerialiseAndDeserialise() throws IOException {
+        // Given
+        final IsXMoreThanY filter = new IsXMoreThanY();
+
+        // When
+        final String json = JsonSerialiser.serialise(filter);
+
+        // Then
+        JsonSerialiser.assertEquals(String.format("{%n" +
+                "  \"class\" : \"uk.gov.gchq.koryphe.impl.predicate.IsXMoreThanY\"%n" +
+                "}"), json);
+
+        // When 2
+        final IsXMoreThanY deserialisedFilter = JsonSerialiser.deserialise(json, IsXMoreThanY.class);
+
+        // Then 2
+        assertNotNull(deserialisedFilter);
+    }
+
+    @Override
+    protected Class<IsXMoreThanY> getPredicateClass() {
+        return IsXMoreThanY.class;
+    }
+
+    @Override
+    protected IsXMoreThanY getInstance() {
+        return new IsXMoreThanY();
+    }
+}
