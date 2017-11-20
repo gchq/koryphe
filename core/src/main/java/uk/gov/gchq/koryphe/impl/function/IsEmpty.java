409d2aa37d01f836569a96903b40f976e52a2725
m607123 <m607123@users.noreply.github.com>
2017-11-20 12:36:37 +0000

    gh-60 - new functions added

---
+++ core/src/main/java/uk/gov/gchq/koryphe/impl/function/IsEmpty.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.koryphe.impl.function;
+
+import com.google.common.collect.Iterables;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import uk.gov.gchq.koryphe.function.KorypheFunction;
+
+public class IsEmpty extends KorypheFunction<Iterable, Boolean> {
+    @Override
+    @SuppressFBWarnings(value = "DE_MIGHT_IGNORE", justification = "Any exceptions are to be ignored")
+    public Boolean apply(final Iterable input) {
+        if (null == input) {
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+        try {
+            return Iterables.isEmpty(input);
+        } finally {
+            if (input instanceof AutoCloseable) {
+                try {
+                    ((AutoCloseable) input).close();
+                } catch (final Exception e) {
+                    // ignore exceptions
+                }
+            }
+        }
+    }
+}
