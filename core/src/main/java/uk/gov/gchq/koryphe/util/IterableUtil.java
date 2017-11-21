d8662bcc7e18cd0f70a0a62c8217c6efeda8dada
m607123 <m607123@users.noreply.github.com>
2017-11-21 17:09:22 +0000

    gh-60 - IterableUtil now a final class

--- core/src/main/java/uk/gov/gchq/koryphe/util/IterableUtil.java
+++ core/src/main/java/uk/gov/gchq/koryphe/util/IterableUtil.java
@@ -22,7 +22,11 @@ import java.util.function.Function;
  * An {@code IterableUtil} is a utility class for lazily applying a {@link Function}
  * to each element of an {@link Iterable}
  */
-public abstract class IterableUtil {
+public final class IterableUtil {
+    private IterableUtil() {
+        // Empty
+    }
+
     public static <I_ITEM, O_ITEM> Iterable<O_ITEM> applyFunction(final Iterable<I_ITEM> input, final Function<I_ITEM, O_ITEM> function) {
         return () -> new Iterator<O_ITEM>() {
             Iterator<? extends I_ITEM> iterator = input.iterator();
