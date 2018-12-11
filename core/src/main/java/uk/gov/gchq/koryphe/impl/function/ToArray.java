b17150baf8bd51071d7962ad90d3fa09ce3b33f9
p013570 <p013570@users.noreply.github.com>
2018-12-11 08:10:12 +0000

    gh-130 - functions to convert objects into list, set and array

---
+++ core/src/main/java/uk/gov/gchq/koryphe/impl/function/ToArray.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.koryphe.impl.function;
+
+import com.google.common.collect.Iterables;
+
+import uk.gov.gchq.koryphe.Since;
+import uk.gov.gchq.koryphe.Summary;
+import uk.gov.gchq.koryphe.function.KorypheFunction;
+
+/**
+ * A {@code ToArray} is a {@link java.util.function.Function} that takes
+ * an Object and converts it to a array. If the object is already an array then it is just returned.
+ * If it is an iterable then the items will be added to a new array. Otherwise a new array is created with
+ * the single value as an item.
+ */
+@Since("1.6.0")
+@Summary("Converts an object into a Array")
+public class ToArray extends KorypheFunction<Object, Object[]> {
+
+    @Override
+    public Object[] apply(final Object value) {
+        if (null == value) {
+            return new Object[]{null};
+        }
+
+        if (value instanceof Object[]) {
+            return ((Object[]) value);
+        }
+
+        if (value instanceof Iterable) {
+            return Iterables.toArray(((Iterable) value), Object.class);
+        }
+
+        return new Object[]{value};
+    }
+}
---
+++ core/src/test/java/uk/gov/gchq/koryphe/impl/function/ToSetTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2018 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.koryphe.impl.function;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import uk.gov.gchq.koryphe.function.FunctionTest;
+import uk.gov.gchq.koryphe.util.JsonSerialiser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+public class ToSetTest extends FunctionTest {
+    @Test
+    public void shouldConvertNullToSet() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = null;
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertEquals(Sets.newHashSet((Object) null), result);
+    }
+
+    @Test
+    public void shouldConvertStringToSet() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = "value1";
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertEquals(Sets.newHashSet(value), result);
+    }
+
+    @Test
+    public void shouldConvertArrayToSet() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = new String[]{"value1", "value2"};
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertEquals(Sets.newHashSet((Object[]) value), result);
+    }
+
+    @Test
+    public void shouldConvertListToSet() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = Lists.newArrayList("value1", "value2");
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertEquals(Sets.newHashSet((List) value), result);
+    }
+
+    @Test
+    public void shouldReturnAGivenSet() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = Sets.newHashSet("value1", "value2");
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertSame(value, result);
+    }
+
+    @Override
+    protected Function getInstance() {
+        return new ToSet();
+    }
+
+    @Override
+    protected Class<? extends Function> getFunctionClass() {
+        return ToSet.class;
+    }
+
+    @Override
+    public void shouldJsonSerialiseAndDeserialise() throws IOException {
+        // Given
+        final ToSet function = new ToSet();
+
+        // When
+        final String json = JsonSerialiser.serialise(function);
+
+        // Then
+        JsonSerialiser.assertEquals(String.format("{%n" +
+                "  \"class\" : \"uk.gov.gchq.koryphe.impl.function.ToSet\"%n" +
+                "}"), json);
+
+        // When 2
+        final ToSet deserialisedMethod = JsonSerialiser.deserialise(json, ToSet.class);
+
+        // Then 2
+        assertNotNull(deserialisedMethod);
+    }
+}
