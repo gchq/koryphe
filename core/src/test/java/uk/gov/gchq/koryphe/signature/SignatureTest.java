470e5549bb33e58410626ee396184860e2a62c7e
m29827 <62667809+m29827@users.noreply.github.com>
2020-05-11 10:00:59 +0100

    gh-184 Improve signature generation, maintaining compatibility with Gaffer tests. (#186)

--- core/src/test/java/uk/gov/gchq/koryphe/signature/SignatureTest.java
+++ core/src/test/java/uk/gov/gchq/koryphe/signature/SignatureTest.java
@@ -35,12 +35,16 @@ import uk.gov.gchq.koryphe.impl.predicate.range.InRange;
 import uk.gov.gchq.koryphe.predicate.MockPredicate2False;
 import uk.gov.gchq.koryphe.predicate.MockPredicateFalse;
 import uk.gov.gchq.koryphe.predicate.MockPredicateTrue;
+import uk.gov.gchq.koryphe.tuple.MapTuple;
+import uk.gov.gchq.koryphe.tuple.Tuple;
 import uk.gov.gchq.koryphe.tuple.function.KorypheFunction2;
 import uk.gov.gchq.koryphe.util.InvalidSignatureTestPredicate;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -48,6 +52,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class SignatureTest {
     @Test
@@ -215,9 +220,6 @@ public class SignatureTest {
 
         assertFalse(input.assignable(Integer.class, Collection.class).isValid());
         assertFalse(input.assignable(Double.class).isValid());
-
-        assertArrayEquals(new Class[]{Object.class}, input.getClasses());
-        assertTrue(input.getNumClasses().equals(1));
     }
 
     @Test
@@ -267,12 +269,14 @@ public class SignatureTest {
 
         // Then
         assertTrue(input.assignable(Collection.class).isValid());
+        assertFalse(input.assignable(Object.class).isValid());
 
         // When
         final Signature output = Signature.getOutputSignature(function);
 
         // Then
         assertTrue(output.assignable(Collection.class).isValid());
+        assertFalse(output.assignable(Object.class).isValid());
     }
 
     @Test
@@ -306,4 +310,133 @@ public class SignatureTest {
         assertFalse(signature.assignable(Integer.class, Double.class, String.class).isValid());
         assertFalse(signature.assignable(Double.class, Long.class).isValid());
     }
+
+    @Test
+    public void shouldGenerateInputSignatureUsingTupleClassWhenNotParameterised() {
+        final Function<TestTuple, String> fromTestTuple = new KorypheFunction<TestTuple, String>() {
+            @Override
+            public String apply(final TestTuple r) {
+                return "something";
+            }
+        };
+
+        final Signature signature = Signature.getInputSignature(fromTestTuple);
+        assertTrue(signature.assignable(TestTuple.class).isValid());
+        assertFalse(signature.assignable(Object.class).isValid());
+    }
+
+    @Test
+    public void shouldGenerateOutputSignatureUsingTupleClassWhenNotParameterised() {
+        final Function<String, TestTuple> toTestTuple = new KorypheFunction<String, TestTuple>() {
+            @Override
+            public TestTuple apply(final String s) {
+                return new TestTuple();
+            }
+        };
+
+        final Signature signature = Signature.getOutputSignature(toTestTuple);
+        assertTrue(signature.assignable(TestTuple.class).isValid());
+        assertFalse(signature.assignable(Object.class).isValid());
+    }
+
+    @Test
+    public void shouldGenerateInputSignatureUsingMapTupleParameterTypes() {
+        final Function<MapTuple<String>, String> fromMapTuple = new KorypheFunction<MapTuple<String>, String>() {
+            @Override
+            public String apply(final MapTuple<String> t) {
+                return "anything";
+            }
+        };
+
+        final Signature signature = Signature.getInputSignature(fromMapTuple);
+        assertTrue(signature.assignable(String.class).isValid());
+        assertTrue(signature.assignable(String.class, String.class).isValid());
+        assertFalse(signature.assignable(Object.class).isValid());
+    }
+
+    @Test
+    public void shouldGenerateOutputSignatureUsingMapTupleParameterTypes() {
+        final Function<String, MapTuple<String>> toMapTuple = new KorypheFunction<String, MapTuple<String>>() {
+            @Override
+            public MapTuple<String> apply(final String s) {
+                return new MapTuple<>();
+            }
+        };
+
+        final Signature signature = Signature.getOutputSignature(toMapTuple);
+        assertTrue(signature.assignable(String.class).isValid());
+        assertTrue(signature.assignable(String.class, String.class).isValid());
+        assertFalse(signature.assignable(Object.class).isValid());
+    }
+
+    private static class TestTuple implements Tuple<String> {
+        private Map<String, Object> values = new HashMap<>();
+
+        @Override
+        public void put(final String reference, final Object value) {
+            values.put(reference, value);
+        }
+
+        @Override
+        public Object get(final String reference) {
+            return values.get(reference);
+        }
+
+        @Override
+        public Iterable<Object> values() {
+            return values.values();
+        }
+    }
+
+    @Test
+    public void shouldBePermissiveWithMockedBinaryOperator() {
+        final BinaryOperator<Integer> mockBinaryOperator = mock(BinaryOperator.class);
+        final Signature signature = Signature.getInputSignature(mockBinaryOperator);
+
+        assertTrue(signature.assignable(Object.class).isValid());
+        assertTrue(signature.assignable(Object.class, Object.class).isValid());
+    }
+
+    @Test
+    public void shouldBePermissiveWithInlineBinaryOperator() {
+        final BinaryOperator<Integer> inlineBinaryOperator = (int1, int2) -> int1 + int2;
+        final Signature signature = Signature.getInputSignature(inlineBinaryOperator);
+
+        assertTrue(signature.assignable(Object.class).isValid());
+        assertTrue(signature.assignable(Object.class, Object.class).isValid());
+    }
+
+    @Test
+    public void shouldCheckTestIntegerBinaryOperatorParameters() {
+        final TestIntegerBinaryOperator testBinaryOperator = new TestIntegerBinaryOperator();
+        final Signature signature = Signature.getInputSignature(testBinaryOperator);
+
+        assertTrue(signature.assignable(Integer.class).isValid());
+        assertFalse(signature.assignable(String.class).isValid());
+        assertFalse(signature.assignable(Integer.class, Integer.class).isValid());
+    }
+
+    @Test
+    public void shouldCheckTestObjectBinaryOperatorParameters() {
+        final TestObjectBinaryOperator objectBinaryOperator = new TestObjectBinaryOperator();
+        final Signature signature = Signature.getInputSignature(objectBinaryOperator);
+
+        assertTrue(signature.assignable(Object.class).isValid());
+        assertFalse(signature.assignable(Object.class, Object.class).isValid());
+    }
+
+    private class TestIntegerBinaryOperator implements BinaryOperator<Integer> {
+        @Override
+        public Integer apply(final Integer integer, final Integer integer2) {
+            return integer + integer2;
+        }
+    }
+
+    private class TestObjectBinaryOperator implements BinaryOperator<Object> {
+        @Override
+        public Object apply(final Object integer, final Object integer2) {
+            return integer.toString().concat(integer2.toString());
+        }
+    }
+
 }
