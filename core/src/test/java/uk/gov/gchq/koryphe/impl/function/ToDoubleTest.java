f2a5682aeb463b4223a1cec6d053c9add76b25b5
p3430233 <61738524+p3430233@users.noreply.github.com>
2021-12-14 08:26:31 -0800

    added ToDouble function with accompanying tests

---
+++ core/src/test/java/uk/gov/gchq/koryphe/impl/function/ToDoubleTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018-2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.koryphe.impl.function;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.gchq.koryphe.function.FunctionTest;
+import uk.gov.gchq.koryphe.util.JsonSerialiser;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ToDoubleTest extends FunctionTest<ToDouble> {
+
+    @Test
+    public void shouldConvertToDouble() {
+        // Given
+        final ToDouble function = new ToDouble();
+
+        // When
+        Object output = function.apply("5.2");
+
+        // Then
+        assertEquals(5.2, output);
+        assertEquals(Double.class, output.getClass());
+    }
+
+    @Test
+    public void shouldReturnNullWhenValueIsNull() {
+        // Given
+        final ToDouble function = new ToDouble();
+
+        // When
+        final Object output = function.apply(null);
+
+        // Then
+        assertNull(output);
+    }
+
+    @Override
+    protected ToDouble getInstance() {
+        return new ToDouble();
+    }
+
+    @Override
+    protected Iterable<ToDouble> getDifferentInstancesOrNull() {
+        return null;
+    }
+
+    @Override
+    protected Class[] getExpectedSignatureInputClasses() {
+        return new Class[] { Object.class };
+    }
+
+    @Override
+    protected Class[] getExpectedSignatureOutputClasses() {
+        return new Class[] { Double.class };
+    }
+
+    @Test
+    @Override
+    public void shouldJsonSerialiseAndDeserialise() throws IOException {
+        // Given
+        final ToDouble function = new ToDouble();
+
+        // When
+        final String json = JsonSerialiser.serialise(function);
+
+        // Then
+        JsonSerialiser.assertEquals(String.format("{%n" +
+                "  \"class\" : \"uk.gov.gchq.koryphe.impl.function.ToDouble\"%n" +
+                "}"), json);
+
+        // When 2
+        final ToDouble deserialisedMethod = JsonSerialiser.deserialise(json, ToDouble.class);
+
+        // Then 2
+        assertNotNull(deserialisedMethod);
+    }
+}
