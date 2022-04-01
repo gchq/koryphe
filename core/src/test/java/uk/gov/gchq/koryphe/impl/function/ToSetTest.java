759a7c4b5724def4811697578a6ae442a99372f3
p3430233 <61738524+p3430233@users.noreply.github.com>
2022-04-01 13:29:15 +0100

    gh-250 added ability to add to TreeSet within ToSet (#252)
    * gh-250 added ability to add to TreeSet within ToSet
    
    * gh-250 added test, changed nested if, removed Class.forName from constructor
    
    * gh-250 added tests
    
    Co-authored-by: t92549 <80890692+t92549@users.noreply.github.com>

--- core/src/test/java/uk/gov/gchq/koryphe/impl/function/ToSetTest.java
+++ core/src/test/java/uk/gov/gchq/koryphe/impl/function/ToSetTest.java
@@ -24,17 +24,22 @@ import uk.gov.gchq.koryphe.function.FunctionTest;
 import uk.gov.gchq.koryphe.util.JsonSerialiser;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ToSetTest extends FunctionTest<ToSet> {
 
     @Test
-    public void shouldConvertNullToSet() {
+    public void shouldConvertNullToHashSet() {
         // Given
         final ToSet function = new ToSet();
 
@@ -46,7 +51,19 @@ public class ToSetTest extends FunctionTest<ToSet> {
     }
 
     @Test
-    public void shouldConvertStringToSet() {
+    public void shouldConvertNullToEmptyTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+
+        // When
+        final Object result = function.apply(null);
+
+        // Then
+        assertEquals(Sets.newTreeSet(), result);
+    }
+
+    @Test
+    public void shouldConvertStringToHashSet() {
         // Given
         final ToSet function = new ToSet();
         final Object value = "value1";
@@ -59,7 +76,49 @@ public class ToSetTest extends FunctionTest<ToSet> {
     }
 
     @Test
-    public void shouldConvertArrayToSet() {
+    public void shouldConvertStringToTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+        final Object value = "value1";
+        final Set expected = new TreeSet(Sets.newHashSet(value));
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldConvertStringToTreeSetUsingfullyQualifiedClassString() throws ClassNotFoundException {
+        // Given
+        final ToSet function = new ToSet("java.util.TreeSet");
+        final Object value = "value1";
+        final Set expected = new TreeSet(Sets.newHashSet(value));
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldConvertStringToTreeSetUsingSimpleClassString() throws ClassNotFoundException {
+        // Given
+        final ToSet function = new ToSet("TreeSet");
+        final Object value = "value1";
+        final Set expected = new TreeSet(Sets.newHashSet(value));
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected, result);
+        }
+
+    @Test
+    public void shouldConvertArrayToHashSet() {
         // Given
         final ToSet function = new ToSet();
         final Object value = new String[] {"value1", "value2"};
@@ -72,7 +131,21 @@ public class ToSetTest extends FunctionTest<ToSet> {
     }
 
     @Test
-    public void shouldConvertListToSet() {
+    public void shouldConvertArrayToTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+        final Object value = new String[] {"value1", "value2"};
+        final Set expected = new TreeSet(Sets.newHashSet((Object[]) value));
+
+        // When
+        Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldConvertListToHashSet() {
         // Given
         final ToSet function = new ToSet();
         final Object value = Lists.newArrayList("value1", "value2");
@@ -85,7 +158,21 @@ public class ToSetTest extends FunctionTest<ToSet> {
     }
 
     @Test
-    public void shouldReturnAGivenSet() {
+    public void shouldConvertListToTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+        final Object value = Lists.newArrayList("value1", "value2");
+        final Set expected = new TreeSet(Sets.newHashSet((List) value));
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldReturnAGivenHashSet() {
         // Given
         final ToSet function = new ToSet();
         final Object value = Sets.newHashSet("value1", "value2");
@@ -95,6 +182,77 @@ public class ToSetTest extends FunctionTest<ToSet> {
 
         // Then
         assertSame(value, result);
+    }
+
+    @Test
+    public void shouldReturnAGivenTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+        final Object value = new TreeSet(Sets.newHashSet("value1", "value2"));
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertSame(value, result);
+    }
+
+    @Test
+    public void shouldConvertTreeSetToHashSet() {
+        // Given
+        final ToSet function = new ToSet(HashSet.class);
+        final Set expected = Sets.newHashSet("value1", "value2");
+        final Set value = new TreeSet(expected);
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected.getClass(), result.getClass());
+        assertNotEquals(value.getClass(), result.getClass());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldConvertHashSetToTreeSet() {
+        // Given
+        final ToSet function = new ToSet(TreeSet.class);
+        final Set value = Sets.newHashSet("value1", "value2");
+        final Set expected = new TreeSet(value);
+
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(expected.getClass(), result.getClass());
+        assertNotEquals(value.getClass(), result.getClass());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldHaveHashSetAsDefault() {
+        // Given
+        final ToSet function = new ToSet();
+        final Object value = "value1";
+
+        // When
+        final Object result = function.apply(value);
+
+        // Then
+        assertEquals(HashSet.class, function.getImplementation());
+        assertEquals(HashSet.class, result.getClass());
+        assertEquals(Sets.newHashSet(value), result);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenUnrecognisedSetImplementationUsed() {
+        // Given
+        final ToSet function = new ToSet(LinkedHashSet.class);
+
+        // When / Then
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> function.apply(null));
+        assertEquals("Unrecognised Set implementation", e.getMessage());
     }
 
     @Override
@@ -121,14 +279,15 @@ public class ToSetTest extends FunctionTest<ToSet> {
     @Override
     public void shouldJsonSerialiseAndDeserialise() throws IOException {
         // Given
-        final ToSet function = new ToSet();
+        final ToSet function = new ToSet(TreeSet.class);
 
         // When
         final String json = JsonSerialiser.serialise(function);
 
         // Then
         JsonSerialiser.assertEquals(String.format("{%n" +
-                "  \"class\" : \"uk.gov.gchq.koryphe.impl.function.ToSet\"%n" +
+                "  \"class\" : \"uk.gov.gchq.koryphe.impl.function.ToSet\",%n" +
+                "  \"implementation\" : \"java.util.TreeSet\"%n" +
                 "}"), json);
 
         // When 2
@@ -136,5 +295,6 @@ public class ToSetTest extends FunctionTest<ToSet> {
 
         // Then 2
         assertNotNull(deserialisedMethod);
+        assertEquals(TreeSet.class, deserialisedMethod.getImplementation());
     }
 }
